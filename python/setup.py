eef8e10f317aea85a4975ab339177b36c64e6fb5
HelloWorldLTY <tianyu.18@intl.zju.edu.cn>
2024-12-30 21:56:46 -0600

    fix sklearn problem

--- python/setup.py
+++ python/setup.py
@@ -15,7 +15,7 @@ setup(name='stg',
       packages=['stg'],
       install_requires=[
           'torch',
-          'sklearn',
+          'scikit-learn',
           'h5py',
           'six',
           'lifelines'
